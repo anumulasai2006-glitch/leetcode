0d55a12720659d11e780f3bbe8d0e75f13c0fb2b
sai sandeep <anumulasai2006@gmail.com>
2026-05-28 15:11:07 +0530

    Add solution to count special characters in a string
    Implemented a method to count special characters based on the ordering of lowercase and uppercase letters.

---
+++ CountTheNumberOfSpecialCharactersII.java
@@ -0,0 +1,53 @@
+/*
+Problem: Count the Number of Special Characters II
+Link: https://leetcode.com/problems/count-the-number-of-special-characters-ii/
+
+Difficulty: Medium
+
+Learnings:
+- Practiced uppercase and lowercase character handling
+- Learned ordering/index based conditions
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public int numberOfSpecialChars(String word) {
+
+        int[] lastLower = new int[26];
+        int[] firstUpper = new int[26];
+
+        for (int i = 0; i < 26; i++) {
+            lastLower[i] = -1;
+            firstUpper[i] = -1;
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+
+            char ch = word.charAt(i);
+
+            if (Character.isLowerCase(ch)) {
+                lastLower[ch - 'a'] = i;
+            } else {
+                if (firstUpper[ch - 'A'] == -1) {
+                    firstUpper[ch - 'A'] = i;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < 26; i++) {
+
+            if (lastLower[i] != -1 &&
+                firstUpper[i] != -1 &&
+                lastLower[i] < firstUpper[i]) {
+
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
